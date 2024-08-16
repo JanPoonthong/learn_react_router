1622edc88f6e22dfdd1a53beb58da27bbe6e9243
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-16 11:08:45 +0700

    feat: passing data through route components

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import Root from "./routes/root";
 import "./index.css";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
