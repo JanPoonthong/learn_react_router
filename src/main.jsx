867b66aeb70f3f65c46ffcf1cad14d4d7d559863
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-16 18:13:40 +0700

    ops: configure router to be deployable

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 import Root, { loader as rootLoader } from "./routes/root";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Root />,
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/learn_react_router/",
+  base: "/routing-app/",
 });
