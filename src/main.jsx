a290bc4cd7eca5c335c47f5df5a18aa2edc564c2
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-16 10:01:47 +0700

    feat: add custom error page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import Root from "./routes/root";
-import "./index.css"
+import "./index.css";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
