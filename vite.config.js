452c739208738134be19fecc4252fa43fc001327
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-16 11:32:10 +0700

    format: prettier format

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/learn_react_router/"
-})
+  base: "/learn_react_router/",
+});
