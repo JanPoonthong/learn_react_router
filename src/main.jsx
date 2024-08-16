65bfce0d353d7334b2e38137893ed70c818a8a4f
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-16 19:39:56 +0700

    feat: edit contact

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createHashRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createHashRouter([
   {
@@ -12,10 +16,18 @@ const router = createHashRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
